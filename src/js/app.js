05fc7ccd86a2e75f312767cdcae81d8bb3e7f4b4
SamuelRosales <samuelrosales.dias@gmail.com>
2025-07-20 21:41:25 -0400

    Reestructuración del backend

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+
+// Configuración de la aplicación
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../", "views")); // ejemplo si está en src/views
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Rutas:::::::::::::::::::::::::::::::::::::::::
+app.use(require('../routes/router.js'));
+app.use(express.static("public"));
+app.use("/assets", express.static(path.join(__dirname, "../../assets")));
+app.use("/src", express.static(path.join(__dirname, "../")));
+app.use(require('../routes/aggInvitado.js'));
+
+//configuración del puerto del servidor
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,() => {
+    if(PORT === 3000) {
+        console.log("Servidor corriendo en el puerto 3000");
+        console.log("http://localhost:3000");
+    } else {
+        console.log(`Servidor corriendo en el puerto ${PORT}`);
+    }
+})
